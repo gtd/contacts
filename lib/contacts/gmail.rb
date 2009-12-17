972cf69ea6974292b37be017a2b50c953b29b0f8
Gabe da Silveira <gabe@websaviour.com>
2009-12-16 23:40:24 -0800

    Extract parse_json method into a higher-level class method so it can be used with yahoo as well

--- lib/contacts/gmail.rb
+++ lib/contacts/gmail.rb
@@ -14,17 +14,6 @@ class Contacts
     LOGIN_REFERER_URL   = "https://www.google.com/accounts/ServiceLogin?service=mail&passive=true&rm=false&continue=http%3A%2F%2Fmail.google.com%2Fmail%2F%3Fui%3Dhtml%26zy%3Dl&bsv=zpwhtygjntrz&scc=1&ltmpl=default&ltmplcache=2"
     CONTACT_LIST_URL    = "https://mail.google.com/mail/contacts/data/contacts?thumb=true&show=ALL&enums=true&psort=Name&max=10000&out=js&rf=&jsx=true"
     PROTOCOL_ERROR      = "Gmail has changed its protocols, please upgrade this library first. If that does not work, dive into the code and submit a patch at http://github.com/cardmagic/contacts"
-
-    def self.parse_json( string )
-      if Object.const_defined?('ActiveSupport') and
-         ActiveSupport.const_defined?('JSON')
-        ActiveSupport::JSON.decode( string )
-      elsif Object.const_defined?('JSON')
-        JSON.parse( string )
-      else
-        raise 'Contacts requires JSON or Rails (with ActiveSupport::JSON)'
-      end
-    end
     
     def real_connect
       postdata = {
@@ -70,7 +59,7 @@ class Contacts
       data.gsub!(/\t/, ' ') # tabs in the note field cause errors with JSON.parse
       data.gsub!(/[\t\x00-\x1F]/, " ") # strip control characters
       
-      @contacts = self.class.parse_json(data)['Body']['Contacts'] || {}
+      @contacts = Contacts.parse_json(data)['Body']['Contacts'] || {}
 
       # Determine in which format to return the data.
       
